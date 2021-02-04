62c3207d2942e45a7b6ac9d10c6060a4301a9192
Abdullah Al Noman <a.noman13@gmail.com>
2021-02-05 00:13:56 +0600

    Update main.go

--- blockdetection/cmd/main/main.go
+++ blockdetection/cmd/main/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
 type NginxBlock struct {
 	StartLine   string
 	EndLine     string
@@ -15,15 +22,17 @@ type NginxBlock struct {
 
 func (ngBlock *NginxBlock) IsBlock(line string) bool {
 	// TODO Solve it using regex
+	startParenthesis,  err := regexp.Match("^\w*|\s*(\{)\w*|\s*$", []byte(ngBlock.StartLine))
+	endParenthesis,  err := regexp.Match("^\w*|\s*(\})\w*|\s*$", []byte(ngBlock.EndLine))
 }
-
-func (ngBlock *NginxBlock) IsLine(line string) bool {
-	// TODO Solve it using regex
-}
-
-func (ngBlock *NginxBlock) HasComment(line string) bool {
-	// TODO Solve it using regex
-}
+//
+//func (ngBlock *NginxBlock) IsLine(line string) bool {
+//	// TODO Solve it using regex
+//}
+//
+//func (ngBlock *NginxBlock) HasComment(line string) bool {
+//	// TODO Solve it using regex
+//}
 
 type NginxBlocks struct {
 	blocks      *[]*NginxBlock
@@ -32,20 +41,33 @@ type NginxBlocks struct {
 	AllLines *[]*string
 }
 
-func GetNginxBlock(
-	lines *[]*string,
-	startIndex,
-	endIndex,
-	recursionMax int,
-) *NginxBlock {
-
-}
-
-func GetNginxBlocks(configContent string) *NginxBlocks {
-
-}
+//func GetNginxBlock(
+//	lines *[]*string,
+//	startIndex,
+//	endIndex,
+//	recursionMax int,
+//) *NginxBlock {
+//
+//}
+//
+//func GetNginxBlocks(configContent string) *NginxBlocks {
+//
+//}
 
 
 func main() {
-	
+	data, _ := ioutil.ReadFile("nginx.config")
+	file := string(data)
+	line := 1
+	var lines []*string
+	temp := strings.Split(file, "\n")
+	for _, item := range temp {
+		//fmt.Println("[", line, "]\t", item)
+		line++
+		lines = append(lines, &item)
+	}
+	for _, item := range lines{
+		fmt.Println(item)
+
+	}
 }
