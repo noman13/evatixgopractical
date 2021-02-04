949569b1978b67f93968a9361e016e1f69567525
Abdullah Al Noman <a.noman13@gmail.com>
2021-02-05 00:54:04 +0600

    regex implement & recursive function call

--- blockdetection/cmd/main/main.go
+++ blockdetection/cmd/main/main.go
@@ -22,17 +22,34 @@ type NginxBlock struct {
 
 func (ngBlock *NginxBlock) IsBlock(line string) bool {
 	// TODO Solve it using regex
-	startParenthesis,  err := regexp.Match("^\w*|\s*(\{)\w*|\s*$", []byte(ngBlock.StartLine))
-	endParenthesis,  err := regexp.Match("^\w*|\s*(\})\w*|\s*$", []byte(ngBlock.EndLine))
+	startParenthesis,  _ := regexp.Match("^\w*|\s*(\{)\w*|\s*$", []byte(ngBlock.StartLine))
+	endParenthesis,  _ := regexp.Match("^\w*|\s*(\})\w*|\s*$", []byte(ngBlock.EndLine))
+	if startParenthesis && endParenthesis {
+		return true
+	} else {
+		return false
+	}
 }
 //
-//func (ngBlock *NginxBlock) IsLine(line string) bool {
-//	// TODO Solve it using regex
-//}
-//
-//func (ngBlock *NginxBlock) HasComment(line string) bool {
-//	// TODO Solve it using regex
-//}
+func (ngBlock *NginxBlock) IsLine(line string) bool {
+	// TODO Solve it using regex
+	chk,  _ := regexp.Match(".*\n$", []byte(ngBlock.StartLine))
+	if chk {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (ngBlock *NginxBlock) HasComment(line string) bool {
+	chk,  _ := regexp.Match("^\#.*", []byte(ngBlock.StartLine))
+	if chk {
+		return true
+	} else {
+		return false
+	}
+	// TODO Solve it using regex
+}
 
 type NginxBlocks struct {
 	blocks      *[]*NginxBlock
@@ -40,34 +57,33 @@ type NginxBlocks struct {
 	// split lines by \n on AllContents
 	AllLines *[]*string
 }
+//
+func GetNginxBlock(
+	lines *[]*string,
+	startIndex,
+	endIndex,
+	recursionMax int,
+) *NginxBlock {
+	return GetNginxBlock(lines, 0, 0,  recursionMax+1)
+}
 
-//func GetNginxBlock(
-//	lines *[]*string,
-//	startIndex,
-//	endIndex,
-//	recursionMax int,
-//) *NginxBlock {
-//
-//}
-//
-//func GetNginxBlocks(configContent string) *NginxBlocks {
-//
-//}
+func GetNginxBlocks(configContent string) *NginxBlocks {
+	//var lines *[]*string := &configContent
+	temp := strings.Split(configContent, "\n")
+	var temp2 []*string
+	for _, item := range temp {
+		temp2 = append(temp2, &item)
+	}
+	GetNginxBlock( &temp2, 0, 0, 0)
+	var obj NginxBlocks
+	return &obj
+}
 
 
 func main() {
 	data, _ := ioutil.ReadFile("nginx.config")
 	file := string(data)
-	line := 1
-	var lines []*string
-	temp := strings.Split(file, "\n")
-	for _, item := range temp {
-		//fmt.Println("[", line, "]\t", item)
-		line++
-		lines = append(lines, &item)
-	}
-	for _, item := range lines{
-		fmt.Println(item)
-
-	}
+	fmt.Println(file)
+	Ngblock := GetNginxBlocks(file)
+	fmt.Println(Ngblock)
 }
