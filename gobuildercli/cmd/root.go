9fbc01c00754ce6860d0d0e03ebcf1b2bf0abc87
Abdullah Al Noman <a.noman13@gmail.com>
2021-02-04 07:16:09 +0600

    -copydir -builddir -exe implemented

--- gobuildercli/cmd/root.go
+++ gobuildercli/cmd/root.go
@@ -57,17 +57,17 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gobuildercli.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "configs", "", "configs file (default is $HOME/.gobuildercli.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
+// initConfig reads in configs file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
+		// Use configs file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
@@ -77,15 +77,15 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".gobuildercli" (without extension).
+		// Search configs in home directory with name ".gobuildercli" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".gobuildercli")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
+	// If a configs file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		fmt.Println("Using configs file:", viper.ConfigFileUsed())
 	}
 }
